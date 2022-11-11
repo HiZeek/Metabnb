0c23a08640c35cc6efe0523bcde43cb53d9e8cb7
Isaac Idibia <idibiaisaac3@gmail.com>
2022-11-11 21:32:32 +0100

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import Brands from '../layout/Brands';
 import Hero from '../layout/Hero';
 import NftSection from '../layout/NftSection';
+import Places from '../layout/Places';
 
 const Home = () => {
   return (
     <Fragment>
         <Hero />
         <Brands />
+        <Places />
         <NftSection />
     </Fragment>
   )
